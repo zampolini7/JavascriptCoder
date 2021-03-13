3383d766f12634919a5829f9734f7448e6fa25b9
Nico Zampolini <zampolini7@gmail.com>
2021-03-13 15:28:33 -0300

    últimas actualizaciones entrega 6

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -25,7 +25,7 @@ saludo();
 
 // PETICIONES POR PROMPT PARA VER OBJETOS
 
-let entradaDeUsuario = prompt(nombrePersona + " para ver nuestros productos escribe: ver \n" + "De lo contrario solo acepta y continua tu recorrido")
+entradaDeUsuario = prompt(nombrePersona + " para ver nuestros productos escribe: ver \n" + "De lo contrario solo acepta y continua tu recorrido")
 //CREADOR DE OBJETOS
 
 class productos1 {
@@ -97,40 +97,40 @@ console.log(listadosDeProductos1)
 // FUNCIÓN PARA SUMAR Y RESTAR COSAS DEL ARRAY
 
 
-alert ("Bievenidos a la calculadora ágil y sencilla");
-let Valor1 = parseInt(prompt("Ingrese un número"));
-let Valor2 = parseInt(prompt("Ingrese un segundo número"));
-let Operacion = prompt ("Ingrese la operacion deseada: suma, resta, multiplicación o división");
+// alert ("Bievenidos a la calculadora ágil y sencilla");
+// let Valor1 = parseInt(prompt("Ingrese un número"));
+// let Valor2 = parseInt(prompt("Ingrese un segundo número"));
+// let Operacion = prompt ("Ingrese la operacion deseada: suma, resta, multiplicación o división");
 
 
 //Me puede servir este if para operar sobre el array, lo veremos más adelante
-function operaciones (){
+// function operaciones (){
 
 
-    if (Operacion == "suma") {
-        let Valor3 = Valor1 + Valor2;
-        console.log ("La suma de los dos números es de = " + Valor3);
-        alert ("La suma de los dos números es de = " + Valor3);
+//     if (Operacion == "suma") {
+//         let Valor3 = Valor1 + Valor2;
+//         console.log ("La suma de los dos números es de = " + Valor3);
+//         alert ("La suma de los dos números es de = " + Valor3);
     
-    }
+//     }
     
-    else if (Operacion == "resta") {
-        let Valor3 = Valor1 - Valor2;
-        console.log ("La resta de los dos números es de = " + Valor3);
-        alert ("La resta de los dos números es de = " + Valor3);
-    }
+//     else if (Operacion == "resta") {
+//         let Valor3 = Valor1 - Valor2;
+//         console.log ("La resta de los dos números es de = " + Valor3);
+//         alert ("La resta de los dos números es de = " + Valor3);
+//     }
     
-    else if (Operacion == "multiplicación" || Operacion == "multiplicacion") {
-        let Valor3 = Valor1 * Valor2;
-        console.log ("La multiplicación de los dos números es de = " + Valor3);
-        alert ("La multiplicación de los dos números es de = " + Valor3);
-    }
+//     else if (Operacion == "multiplicación" || Operacion == "multiplicacion") {
+//         let Valor3 = Valor1 * Valor2;
+//         console.log ("La multiplicación de los dos números es de = " + Valor3);
+//         alert ("La multiplicación de los dos números es de = " + Valor3);
+//     }
     
-    else {
-        console.log("Refresque la página y vuelva a intentarlo")
-        alert ("Refresque la página y vuelva a intentarlo")
-    }
+//     else {
+//         console.log("Refresque la página y vuelva a intentarlo")
+//         alert ("Refresque la página y vuelva a intentarlo")
+//     }
     
     
 
-}    
+// }    
