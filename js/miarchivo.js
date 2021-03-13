0cd4f686a22edcff34b02ca81c485779da0c88dc
Nico Zampolini <zampolini7@gmail.com>
2021-03-13 13:59:08 -0300

    Actualizaciones de entregas de la sexta clase, incluyendo actividad complementaria y primer entrega trabajo final

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,6 +1,14 @@
 //--- VARIABLES GLOBALES ----
 let nombrePersona = "";
+let entradaDeUsuario = "";
 
+
+
+// CREACIÓN DE ARRAYS
+
+const listadoDeProductos = [];
+const CarritoDeproductos = [];
+const CarritoParaOperaciones = [];
 
 
 
@@ -15,9 +23,9 @@ saludo();
 
 
 
-// PETICIONES POR PROMPT PARA CREAR OBJETO
+// PETICIONES POR PROMPT PARA VER OBJETOS
 
-let entradaDeUsuario = prompt(nombrePersona + " para crear un objeto ingresa: crear")
+let entradaDeUsuario = prompt(nombrePersona + " para ver nuestros productos escribe: ver \n" + "De lo contrario solo acepta y continua tu recorrido")
 //CREADOR DE OBJETOS
 
 class productos1 {
@@ -40,21 +48,12 @@ class productos1 {
 
 
 
-if( entradaDeUsuario == "crear") {
-    listadoDeProductos.push(new productos1 ("DeliBox",2600))
-    console.log (listadoDeProductos);
 
 
-   
-    
-}
 
-// CREACIÓN DE ARRAY
-
-const listadoDeProductos = [];
 
 // METIENDO LOS OBJETOS CREADOS AL ARRAY
-
+listadoDeProductos.push (new productos1 ("DeliBox",2600))
 listadoDeProductos.push (new productos1("ParriBox", 2500));
 listadoDeProductos.push (new productos1('teaTime', 2600));
 listadoDeProductos.push (new productos1('movieNigth', 2400));
@@ -69,15 +68,69 @@ listadoDeProductos.push (new productos1('provoBox', 2900));
 listadoDeProductos.push (new productos1('wineBox', 2500));
 
 
-// FOR PARA RECORRER LOS OBJETOS CREADOS
 
+
+// FOR PARA RECORRER LOS OBJETOS CREADOS
+let mensaje = "";
 for (let i of listadoDeProductos){
-    console.log(i);
+    mensaje += " Box: " + i.nombre + ", Precio: "+ i.precio + "\n" ;
+}
+
+if( entradaDeUsuario == "ver"|| entradaDeUsuario == "VER") {
+    
+    console.log (listadoDeProductos);
+    alert ( "Estas son nuestras box disponibles: \n" + mensaje);
+
+
+   
+    
 }
 
 
+// -- ACTIVIDAD COMPLEMENTARIA DE ORDENAR EL ARRAY, VOY A REALIZARLO DE MENOR A MAYOR BASADO EN LOS PRECIOS //
+
+let listadosDeProductos1 = listadoDeProductos;
+listadosDeProductos1 = listadosDeProductos1.sort((a,b)=>a.precio-b.precio); 
+console.log(listadosDeProductos1)
 
 
+// FUNCIÓN PARA SUMAR Y RESTAR COSAS DEL ARRAY
 
 
+alert ("Bievenidos a la calculadora ágil y sencilla");
+let Valor1 = parseInt(prompt("Ingrese un número"));
+let Valor2 = parseInt(prompt("Ingrese un segundo número"));
+let Operacion = prompt ("Ingrese la operacion deseada: suma, resta, multiplicación o división");
 
+
+//Me puede servir este if para operar sobre el array, lo veremos más adelante
+function operaciones (){
+
+
+    if (Operacion == "suma") {
+        let Valor3 = Valor1 + Valor2;
+        console.log ("La suma de los dos números es de = " + Valor3);
+        alert ("La suma de los dos números es de = " + Valor3);
+    
+    }
+    
+    else if (Operacion == "resta") {
+        let Valor3 = Valor1 - Valor2;
+        console.log ("La resta de los dos números es de = " + Valor3);
+        alert ("La resta de los dos números es de = " + Valor3);
+    }
+    
+    else if (Operacion == "multiplicación" || Operacion == "multiplicacion") {
+        let Valor3 = Valor1 * Valor2;
+        console.log ("La multiplicación de los dos números es de = " + Valor3);
+        alert ("La multiplicación de los dos números es de = " + Valor3);
+    }
+    
+    else {
+        console.log("Refresque la página y vuelva a intentarlo")
+        alert ("Refresque la página y vuelva a intentarlo")
+    }
+    
+    
+
+}    
